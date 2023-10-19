43a3edf1360888fdd1a364e680027cc1201ed489
Prezcoder <fbouchar@student.42quebec.com>
2023-10-19 14:57:55 -0400

    MAJ

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -9,12 +9,9 @@ int main (int argc, char **argv)
 {
 	if (argc == 1)
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; argv[i]; i++){	
-		for (int j = 0; argv[i][j]; j++){
-				argv[i][j] = std::toupper(argv[i][j]);
-				cout << argv[i][j];
-			}
-	}
+	for (int i = 1; argv[i]; i++)
+		for (int j = 0; argv[i][j]; j++)
+				cout << (char) toupper(argv[i][j]);
 	cout << endl;
 	return 0;
 }
