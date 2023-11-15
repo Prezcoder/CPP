f2d74661191e97ed1f946ea4861a223bf974ca96
Prezcoder <fbouchar@student.42quebec.com>
2023-11-15 12:35:40 -0500

    MAJ

--- CPP04/ex02/main.cpp
+++ CPP04/ex02/main.cpp
@@ -6,61 +6,77 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
-int main()
-{
-// const AAnimal* meta = new AAnimal(); 
-const AAnimal* j = new Dog();
-const AAnimal* i = new Cat();
-cout << "----------------------------------------" << endl;
-cout << j->getType() << " " << endl;
-cout << i->getType() << " " << endl;
-cout << "----------------------------------------" << endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-cout << "----------------------------------------" << endl;
-const WrongAnimal *meta3 = new WrongAnimal();
-const WrongAnimal *i3 = new WrongCat();
-cout << "----------------------------------------" << endl;
-cout << meta3->getType() << " " << endl;
-cout << i3->getType() << " " << endl;
-cout << "----------------------------------------" << endl;
-meta3->makeSound();
-i3->makeSound();
-cout << "----------------------------------------" << endl;
-Cat cat;
-cat = Cat();
-cout << cat.getBrain()->getIdeas(7) << endl;
-cout << "----------------------------------------" << endl;
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
 
-const AAnimal	*animal_array[4];
-cout << endl;
-// Half filled with dogs
-for (int i = 0; i < 2; i++)
-	animal_array[i] = new Dog();
-cout << endl;
-// Half filled with cats
-for (int i = 2; i < 4; i++)
-	animal_array[i] = new Cat();
-cout << endl;
+void printTitle(const std::string& title) {
+	std::cout << BLUE << "----------------------------------------" << std::endl;
+	std::cout << GREEN << title << RESET << std::endl;
+	std::cout << BLUE << "----------------------------------------" << RESET << std::endl;
+}
 
-// Make them speak
-for (int i = 0; i < 4; i++)
-	animal_array[i]->makeSound();
-cout << endl;
+int main() {
+	// Test 1
+	printTitle("Test 1: Animal Types");
+	const AAnimal* j = new Dog();
+	const AAnimal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
 
-// See if they are really dogs or cats
-for (int i = 0; i < 4; i++)
-	cout << animal_array[i]->getType() << endl;
-cout << endl;
+	// Test 2
+	printTitle("Test 2: Animal Sounds");
+	i->makeSound(); // will output the cat sound!
+	j->makeSound();
 
-// Delete them
-for (int i = 0; i < 4; i++)
-	delete animal_array[i];
-cout << endl;
+	// Test 3
+	printTitle("Test 3: Wrong Animal Types");
+	const WrongAnimal* meta3 = new WrongAnimal();
+	const WrongAnimal* i3 = new WrongCat();
+	std::cout << meta3->getType() << " " << std::endl;
+	std::cout << i3->getType() << " " << std::endl;
 
-delete j;
-delete i;
-delete meta3;
-delete i3;
-return 0;
+	// Test 4
+	printTitle("Test 4: Wrong Animal Sounds");
+	meta3->makeSound();
+	i3->makeSound();
+
+	// Test 5
+	printTitle("Test 5: Cat's Brain Ideas");
+	Cat cat;
+	cat = Cat();
+	std::cout << cat.getBrain()->getIdeas(7) << std::endl;
+
+	// Test 6
+	printTitle("Test 6: Animal Array");
+	const AAnimal* animal_array[4];
+	// Half filled with dogs
+	for (int i = 0; i < 2; i++)
+		animal_array[i] = new Dog();
+
+	// Half filled with cats
+	for (int i = 2; i < 4; i++)
+		animal_array[i] = new Cat();
+
+	// Make them speak
+	for (int i = 0; i < 4; i++)
+		animal_array[i]->makeSound();
+
+	// See if they are really dogs or cats
+	for (int i = 0; i < 4; i++)
+		std::cout << animal_array[i]->getType() << std::endl;
+
+	// Delete them
+	for (int i = 0; i < 4; i++)
+		delete animal_array[i];
+
+	// Cleanup
+	delete j;
+	delete i;
+	delete meta3;
+	delete i3;
+
+	return 0;
 }
